0d339ba40744c5f9379f4825da170f6b4a8ed27c
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-10-02 19:43:22 -0700

    assets folder

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+var question1 = document.getElementById("answer1");
+var question2 = document.getElementById("answer2");
+var comment = document.getElementById("msg");
+var saveButton = document.getElementById("save");
+var savedAnswer = document.getElementById("saved-question");
+
+function saveAnswers() {
+  // Save related form data as an object
+  var questionAnswers = {
+    question1: question1.value,
+    question2: question2.value,
+    comment: comment.value.trim()
+  };
+  // Use .setItem() to s
+  localStorage.setItem("questionsAnswers", JSON.stringify(questionAnswers));
+}
+
+
+function renderLastGrade() {
+ 
+  var lastGrade = JSON.parse(localStorage.getItem(questionsAnswers));
+
+  if (lastGrade !== null) {
+  document.getElementById("saved-question").innerHTML = lastGrade.student;
+  document.getElementById("saved-questions").innerHTML = lastGrade.grade;
+  document.getElementById("saved-comment").innerHTML = lastGrade.comment;
+
+  } else {
+    return;
+  }
+}
+
+saveButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  saveLastGrade();
+  renderLastGrade();
+});
+
+ 
+function init() {
+
+  renderLastGrade();
+}
+init();
